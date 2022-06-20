7d45b0083cc1235b185a91b1d2218d84003fb903
Nadir Nadirli <companynadirnadirli@gmail.com>
2022-06-20 19:30:51 +0400

    Using ID and IoC

---
+++ src/test/java/nanemo/springframework/sfgid/SfgDIApplicationTests.java
@@ -0,0 +1,13 @@
+package nanemo.springframework.sfgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
